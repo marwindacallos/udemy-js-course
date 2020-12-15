c80869ab03bac8c72a9bf586ce7f5aa28dccd4b4
Marwin Dacallos <71876693+marwindacallos@users.noreply.github.com>
2020-12-16 02:30:24 +0800

    Updating Js Course

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/ass.js
@@ -0,0 +1,45 @@
+// Values and Variables
+
+let country = 'Philippines';
+let continent = 'Asia';
+let phPopulation = 106700000
+
+// console.log(`I am living in ${country}, located on ${continent} and we have population of ${population}`);
+
+// Data Types
+let isIsland = 'Philippines';
+isIsland = true;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof country);
+// console.log(typeof continent);
+
+// LET CONST VAR
+
+let language = 'Filipino';
+
+
+// console.log(language, firstName);
+
+language = 'English';
+// console.log(language);
+
+// BASIC OPERATORS
+
+let halfCountry = phPopulation / 2
+// console.log(`Each half of ${country} will have ${halfCountry} of population`); 
+
+halfCountry++;
+// console.log(halfCountry);
+
+let finPopulation = 6000000
+if (finPopulation > phPopulation){
+ console.log(`No`);
+}
+else {
+ // console.log(`Yes, my population is bigger`);
+}
+
+let description = 'Portugal is in Europe, and its 11 million people speak portugues'
+// console.log(description);
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/challenges.js
@@ -0,0 +1,29 @@
+// CODING CHALLENGE 1
+
+ // DATA 1
+
+ let markWeight = 78;
+ let markHeight = 1.69; 
+ let johnWeight = 92; 
+ let johnHeight = 1.95; 
+
+ let markBMI = (markWeight / (markHeight ** 2));
+ let johnBMI = (johnWeight / (johnHeight ** 2));
+
+ let markHigherBMI = (markBMI > johnBMI)
+
+ console.log(markHigherBMI);
+
+ // DATA 2
+
+ let markWeight2 = 95;
+ let markHeight2 = 1.88; 
+ let johnWeight2 = 85; 
+ let johnHeight2 = 1.76; 
+
+ let markBMI2 = (markWeight2 / (markHeight2 ** 2));
+ let johnBMI2 = (johnWeight2 / (johnHeight2 ** 2));
+
+ let markHigherBMI2 = (markBMI2 > johnBMI2)
+
+ console.log(markHigherBMI2);
