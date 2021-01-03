e65469f37c56dd6e77d2a9fd157c4b89f6292058
Marwin Dacallos <71876693+marwindacallos@users.noreply.github.com>
2021-01-03 23:32:18 +0800

    Updating JS Course

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/main.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/main.js
@@ -338,15 +338,35 @@ const jonas = [
 // 0, 1 , ... 4
 // 4, 3, ..., 0
 
-for(let i = jonas.length - 1; i >= 0 ; i--){
- console.log(jonas[i]);
-}
+// for(let i = jonas.length - 1; i >= 0 ; i--){
+//  console.log(jonas[i]);
+// }
 
-for (let exercise = 1; exercise < 4; exercise++){
- console.log(`---------Starting exercise ${exercise}`);
+// for (let exercise = 1; exercise < 4; exercise++){
+//  console.log(`---------Starting exercise ${exercise}`);
 
- for (let rep= 1; rep <6; rep++){
- console.log(`Lifting weight repetition ${rep}`);
-}
+//  for (let rep= 1; rep <6; rep++){
+//  console.log(`Lifting weight repetition ${rep}`);
+// }
+// }
 
+/////////////////////////// WHILE LOOP
+
+// for(let rep = 1; rep <= 10; rep++){
+//  console.log(`Lifting weights ${rep}`);
+// }
+
+// let rep = 1;
+// while (rep <=10){
+//  console.log(`WHILE: Lifting weights ${rep}`);
+//  rep++;
+// }
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !==6){
+ console.log(`Your rolled a ${dice}`);
+ dice = Math.trunc(Math.random() * 6) + 1;
+
+ if (dice === 6) console.log(`Loop is about to end...`);
 }
