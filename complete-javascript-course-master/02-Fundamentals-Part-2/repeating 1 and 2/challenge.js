3781c5692b7b2b3ffdba8066467635d8c02641fe
Marwin Dacallos <71876693+marwindacallos@users.noreply.github.com>
2021-01-09 21:38:59 +0800

    Updating JS Course

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/repeating 1 and 2/challenge.js
@@ -0,0 +1,46 @@
+'use strict';
+
+///////////////////// PART 1
+
+////////////////////////CODING CHALLENGE #1 & #2
+
+// const markBMI = 95 / 1.88 ** 2
+// const johnBMI = 85 / 1.76 ** 2
+
+// let markHigherBMI;
+// if (markBMI > johnBMI){
+//  markHigherBMI = true;
+//  console.log(`"Mark's BMI is higher than John's!"`);
+// } else{
+//  markHigherBMI = false;
+//  console.log(`"John's BMI is higher than Mark's!"`);
+// }
+
+// console.log(markHigherBMI);
+
+/////////////////////////CODING CHALLENGE #3
+
+// const dolphinAverage = (97 + 90+ 101)/ 3
+// const koalasAverage = (109 + 150 + 106)/ 3
+
+// if (dolphinAverage === koalasAverage || dolphinAverage >= 100 && koalasAverage >= 100){
+//  console.log(`Draw`);
+// } else if (dolphinAverage > koalasAverage && dolphinAverage >= 100){
+//  console.log(`Dolphin won`);
+// } else if (koalasAverage > dolphinAverage && koalasAverage >= 100){
+//  console.log(`Koala won`);
+// } else{
+//  console.log(`No one wins`);
+// }
+
+/////////////////////////////CODING CHALLENGE #4
+
+// const bill = 275
+// let tip;
+// if (50 <= bill <= 300){
+//  tip = bill * .15
+// } else{
+//  tip = bill * .2
+// }
+// const totalValue = bill + tip
+// console.log(`The bill was ${bill}, the tip was ${tip} and the total value ${totalValue} `);
