82b2de9715a9fde5d51133b56d0dbaab4262e014
Marwin Dacallos <71876693+marwindacallos@users.noreply.github.com>
2021-02-04 20:25:56 +0800

    Updating JS Course

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -49,6 +49,76 @@ const restaurant = {
   },
 };
 
+//MAPS
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+// console.log(rest.size);
+
+// console.log(rest.get(arr));
+///////////////////////////////////
+//SETS
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+
+// console.log(ordersSet);
+
+// console.log(new Set('Jonas'));
+
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('Pizza'));
+// console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+// console.log(ordersSet);
+
+// for (const order of ordersSet) console.log(order);
+
+//Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// console.log(staff);
+// const staffUnique = new Set(staff);
+const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
+// console.log(
+//   new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+// );
+
+// console.log(new Set('jonasschemedtmann').size);
+
 // property NAMES
 
 // const properties = Object.keys(openingHours);
@@ -71,7 +141,7 @@ const restaurant = {
 
 // //Entire Object
 // const entries = Object.entries(openingHours);
-// // console.log(entries);
+// console.log(entries);
 
 // for (const [key, { open, close }] of entries) {
 //   console.log(`On ${key} we open at ${open} and close at ${close}`);
@@ -171,7 +241,7 @@ const guest2 = restaurant.numGuest || 10;
 // Destructuring
 
 // SPREAD, because on RIGHT side of =
-const arr = [1, 2, ...[3, 4]];
+// const arr = [1, 2, ...[3, 4]];
 
 //REST, because on Left side of =
 const [a, b, ...others] = [1, 2, 3, 4, 5];
@@ -193,7 +263,7 @@ const add = function (...numbers) {
   // console.log(numbers);
   let sum = 0;
   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
-  console.log(sum);
+  // console.log(sum);
 };
 
 // add(2, 3);
