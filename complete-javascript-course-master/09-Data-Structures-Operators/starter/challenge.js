0469e6ee8d35d136a1b6b42d6001e5e7d2d2c58d
Marwin Dacallos <71876693+marwindacallos@users.noreply.github.com>
2021-02-05 18:33:07 +0800

    Updating JS Course

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/challenge.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/challenge.js
@@ -235,3 +235,66 @@ const game = {
 //   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
 // console.log(`Odd of ${teamStr} ${odd}`);
 // }
+
+//////////////////////////////////////////////////////
+// Challenge #3
+
+// Let's continue with our football betting app! This time, we have a map called
+// 'gameEvents' (see below) with a log of the events that happened during the
+// game. The values are the events themselves, and the keys are the minutes in which
+// each event happened (a football game has 90 minutes plus some extra time).
+
+// Your tasks:
+// 1. Create an array 'events' of the different game events that happened (no
+// duplicates)
+
+// 2. After the game has finished, is was found that the yellow card from minute 64
+// was unfair. So remove this event from the game events log.
+
+// 3. Compute and log the following string to the console: "An event happened, on
+// average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+// 4. Loop over 'gameEvents' and log each element to the console, marking
+// whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽ GOAL
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// const hey = new Set(Object.entries(gameEvents));
+// console.log(hey);
+
+//1.
+// console.log(gameEvents.values());
+// // const events = new Set(gameEvents.values());
+// const events = [...new Set(gameEvents.values())]; //To convert to array
+// console.log(events);
+
+//2.
+// gameEvents.delete(64);
+// console.log(gameEvents);
+
+//3.
+// const timez = [...gameEvents.keys()].pop(); //remove the last and return the value
+// console.log(timez);
+
+// console.log(
+//   `"An event happened, on average, every ${time / gameEvents.size} minutes"`
+// );
+
+//4.
+// for (const [min, event] of gameEvents) {
+//   const half = min <= 45 ? 'FIRST' : 'SECOND';
+//   console.log(`[${half} HALF] ${min}: ${event}`);
+// }
