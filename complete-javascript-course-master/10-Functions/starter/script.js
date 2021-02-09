95c8940449795ea06e42c15e39335e36bff985bc
Marwin Dacallos <71876693+marwindacallos@users.noreply.github.com>
2021-02-09 21:52:12 +0800

    Updating JS Course

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const booking = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const books = {
+    flightNum: flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(books);
+  booking.push(books);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000);
