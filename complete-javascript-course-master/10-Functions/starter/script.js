2419cdc59c27720bed9c7cd30cd3833ef851f9b3
Marwin Dacallos <71876693+marwindacallos@users.noreply.github.com>
2021-02-10 22:34:36 +0800

    Updating JS Course

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -20,9 +20,23 @@ const createBooking = function (
   booking.push(books);
 };
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 2);
-createBooking('LH123', 5);
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+// createBooking('LH123', undefined, 1000);
 
-createBooking('LH123', undefined, 1000);
+// Passing Arguments Works Value vs Reference
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schemdtmann',
+  passport: 123456789,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+};
+
+checkIn(flight, jonas);
